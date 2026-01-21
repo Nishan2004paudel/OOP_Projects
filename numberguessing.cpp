c5643bf44aa58bad267cdce2ecc0ce13c5a9dca9
Nishan Paudel <neezpau04@gmail.com>
2026-01-21 18:59:56 +0545

    feat: build a simple number guessing game

---
+++ numberguessing.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    srand(time(0));
+
+    int secretNumber = rand()% 100 + 1;
+    int guess;
+    int attempts = 0;
+    cout<<"Welcome to Number Guessing Game! "<< endl;
+    cout<<"I have chosen a number between 1 and 100"<< endl;
+
+    while(true){
+        cout<< "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+        if (guess > secretNumber){
+            cout<< "Too high!"<< endl;
+        }
+        else if(guess < secretNumber){
+            cout<< "Too Low!"<< endl;
+        }
+        else{
+            cout<< "Correct! You guessed it in "<<attempts<< " attempts. "<<endl;
+            break;
+
+        }
+    }
+    return 0;
+}
